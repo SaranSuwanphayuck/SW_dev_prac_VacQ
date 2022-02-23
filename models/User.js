880a9ae24dea9358d3eeabc22064145d11bd195e
puiza <saran.swpy@gmail.com>
2022-02-24 04:21:35 +0700

    add authentication

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name.']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email.'],
+        unique: true,
+        match: [/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/, 'Please add a valid email.']
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password.'],
+        minlength: 6,
+        select: false
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+//Encrypt password using bcrypt
+UserSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+//Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function() {
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    })
+}
+
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const res = require('express/lib/response');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
 
 //Load variables
 dotenv.config({path:'./config/config.env'});
@@ -11,7 +12,7 @@ connectDB();
 
 //Routes
 const hospitals = require('./routes/hospitals');
-
+const auth = require('./routes/auth');
 
 
 const app = express();
@@ -26,8 +27,11 @@ app.get('/', (req, res) => {
         }
     })
 })
+//cookie-parser
+app.use(cookieParser());
 
 app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/auth', auth);
 
 
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`));
